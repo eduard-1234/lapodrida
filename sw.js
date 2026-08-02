e9e06c706785e6212014dd51c1fdf9778c87f21c
eduard-1234 <agusti449@gmail.com>
2026-08-02 16:08:31 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE = "lapodrida-v1";
+const LOCAL = ["./", "./index.html", "./manifest.webmanifest", "./icon-180.png", "./icon-512.png"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE).then((c) =>
+      Promise.all(LOCAL.map((u) => c.add(u).catch(() => {})))
+    ).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+// Primer la memòria cau; si no hi és, xarxa (i es guarda per la propera)
+self.addEventListener("fetch", (e) => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(
+    caches.match(e.request).then((hit) => {
+      if (hit) return hit;
+      return fetch(e.request).then((res) => {
+        const copy = res.clone();
+        caches.open(CACHE).then((c) => c.put(e.request, copy)).catch(() => {});
+        return res;
+      });
+    })
+  );
+});
